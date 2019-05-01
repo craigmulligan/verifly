b07bb609c336d22419b2c109bc6f629a98e434d8
Craig Mulligan <hobochildster@gmail.com>
2019-05-01 19:28:20 +0100

    twerks

---
+++ README.md
@@ -0,0 +1,47 @@
+## Verifly
+
+> Domain verification as a service.
+
+There are two ways to verify a domain.
+
+1. Start a worker to verify a dns `txt` record. It will try for 30 minutes with exponential backoff:
+
+```
+curl -X POST http://localhost:8080/worker -H 'Content-Type: application/json' -d @data.json
+```
+
+2. Directly call a challenge:
+
+```
+curl -X POST http://localhost:8080/worker -H 'Content-Type: application/json' -d @data.json
+```
+
+The payload for both endpoints is the same.
+
+```json
+{
+  // The domain you want to verify
+  "domain": "transparently.app",
+  // The callback url where we post success or failure.
+  "callback_url": "http://localhost:3000",
+  // The unique challenge the user should update their txt records with. (optional)
+  "challenge": "808bdb2e-8047-11e9-8aa5-9cb6d089854f"
+}
+```
+
+The `callback_url` is called with the following payload:
+
+```json
+{
+  // The domain you want to verify
+  "domain": "transparently.app",
+  // The callback url where we post success or failure.
+  "callback_url": "http://localhost:3000",
+  // The unique challenge  - the user should update their txt records with. (optional)
+  "challenge": "808bdb2e-8047-11e9-8aa5-9cb6d089854f"
+  // Whether the txt record was found and the domain is therefore verified.
+  "is_verified": true
+}
+```
+
+**NOTE** This is a fun project to explore golang. If you'd like to suggest improvements on my golang or report a bug please [open an issue](issues/new).
--- verifly.go
+++ verifly.go
@@ -1,39 +1,147 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"encoding/json"
-	"fmt"
+	"github.com/google/uuid"
+	"google.golang.org/appengine" // Required external App Engine library
+	"google.golang.org/appengine/taskqueue"
+	"google.golang.org/appengine/urlfetch"
 	"log"
 	"net"
 	"net/http"
+	"time"
 )
 
-func lookupRecord() {
-	txtrecords, _ := net.LookupTXT("basis-test.bike")
+type Record struct {
+	Domain      string `json:"domain"`
+	Challenge   string `json:"challenge"`
+	Verified    bool   `json:"verified"`
+	CallbackUrl string `json:"callback_url"`
+}
+
+func lookupRecord(record *Record) bool {
+	txtrecords, err := net.LookupTXT(record.Domain)
+
+	if err != nil {
+		log.Fatalln(err)
+		return false
+	}
 
 	for _, txt := range txtrecords {
-		fmt.Println(txt)
+		if txt == record.Challenge {
+			return true
+		}
+
 	}
+
+	return false
 }
 
-type payload struct {
-	Domain       string
-	Callback_url string
-}
+func notify(rw *http.Request, payload Record) (*http.Response, error) {
+	body := new(bytes.Buffer)
+	json.NewEncoder(body).Encode(payload)
 
-func route(rw http.ResponseWriter, req *http.Request) {
-	decoder := json.NewDecoder(req.Body)
-	var p payload
-	err := decoder.Decode(&p)
+	req, err := http.NewRequest("POST", payload.CallbackUrl, body)
+
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 
-	log.Println(p.Domain)
+	req.Header.Add("Content-Type", "application/json")
+
+	ctx := appengine.NewContext(rw)
+	client := urlfetch.Client(ctx)
+
+	return client.Do(req)
+}
+
+func createChallenge() (string, error) {
+	id, err := uuid.NewUUID()
+	return id.String(), err
+}
+
+func PostTask(record Record) *taskqueue.Task {
+	h := make(http.Header)
+	h.Set("Content-Type", "application/json")
+	payload, _ := json.Marshal(record)
+	ageLimit, _ := time.ParseDuration("20m")
+	minBackoff, _ := time.ParseDuration("5s")
+
+	return &taskqueue.Task{
+		Path:    "/challenge",
+		Payload: payload,
+		Header:  h,
+		Method:  "POST",
+		RetryOptions: &taskqueue.RetryOptions{
+			AgeLimit:   ageLimit,
+			MinBackoff: minBackoff,
+		},
+	}
+}
+
+func scheduleTask(record Record, ctx context.Context) (*taskqueue.Task, error) {
+	t := PostTask(record)
+
+	return taskqueue.Add(ctx, t, "")
+}
+
+func worker(rw http.ResponseWriter, req *http.Request) {
+	ctx := appengine.NewContext(req)
+
+	decoder := json.NewDecoder(req.Body)
+	var record Record
+
+	err := decoder.Decode(&record)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if record.Domain == "" {
+		challenge, _ := createChallenge()
+		record.Challenge = challenge
+	}
+
+	_, err = scheduleTask(record, ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	rw.WriteHeader(http.StatusOK)
+	json.NewEncoder(rw).Encode(record)
+}
+
+func challenge(rw http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+	var record Record
+
+	err := decoder.Decode(&record)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("Worker challenge", record.Challenge)
+	record.Verified = lookupRecord(&record)
+
+	// Finished
+	if record.Verified {
+		res, err := notify(req, record)
+		if err != nil {
+			rw.WriteHeader(res.StatusCode)
+			return
+		}
+		log.Printf("Worker complete", record.Challenge)
+		rw.WriteHeader(http.StatusOK)
+		return
+	}
+
+	rw.WriteHeader(http.StatusNotFound)
+	json.NewEncoder(rw).Encode(record)
 }
 
 func main() {
-	http.HandleFunc("/", route)
-
-	http.ListenAndServe(":2000", nil)
+	http.HandleFunc("/worker", worker)
+	http.HandleFunc("/challenge", challenge)
+	appengine.Main()
 }
